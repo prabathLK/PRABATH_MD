985290d1bd6f168072a65d37920c50a6cb5b59e9
SL Prabath Official <106251140+prabathLK@users.noreply.github.com>
2023-01-24 09:40:04 +0530

    Update _help.js

--- commands/_help.js
+++ commands/_help.js
@@ -23,7 +23,7 @@ Secktor.cmd({
             alias: ["menu"],
             desc: "Help list",
             category: "general",
-            react: "✨",
+            react: "🔖",
             filename: __filename
         },
         async(Void, citel, text) => {
