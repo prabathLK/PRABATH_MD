628de5daf066e3a0f70ba0d7de78696b4940bfe7
SL Prabath Official <106251140+prabathLK@users.noreply.github.com>
2023-01-24 11:01:25 +0530

    Update _help.js

--- commands/_help.js
+++ commands/_help.js
@@ -52,15 +52,15 @@ Secktor.cmd({
                     .locale('id')
                 const date = moment.tz('Asia/Kolkata').format('DD/MM/YYYY')
                 let total = await sck1.countDocuments()
-                let str = 
+                let str = `╭────🔖 ` + fancytext(Config.ownername.split(' ')[0], 58) + ` 🔖\n`
                 str +=
-                    '```' + ` ╭───────✧👨‍💻✧───────❖
+                    '```' + `│ ╭───────✧👨‍💻✧───────❖
  │ Uptime:- ${runtime(process.uptime())}
  │ Mem:- ${formatp(os.totalmem() - os.freemem())}/${formatp(os.totalmem())}
  │ Time:- ${time}
  │ Date:- ${date}
  ╰───────✧👨‍💻✧───────❖
-  ❖───────────────❖\n
+    ❖───────────────❖\n
 ` + '```'
                 str += `╭───『 ` + fancytext('Commands', 57) + `』──◆`
                 for (const category in cmds) {
