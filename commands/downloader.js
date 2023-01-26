a40ece960eaa6550fc59327bec08591c76c7e387
SL Prabath Official <106251140+prabathLK@users.noreply.github.com>
2023-01-26 23:48:05 +0530

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -521,7 +521,7 @@ cmd({
                 let yts = require("secktor-pack");
                 let search = await yts(text);
             await Void.sendMessage(citel.chat, 
-    { document: fs.readFileSync(`./${randomName}`), mimetype: 'audio/mpeg' },{quoted: citel})
+    { document: fs.readFileSync(`./${randomName}`), mimetype: 'audio/mp4' },{quoted: citel})
             } else {
                 citel.reply(`❌ File size bigger than 40mb.`);
             }
