e04f5c387826de181c2e7b043a04f65e4beadba8
SL Prabath Official <106251140+prabathLK@users.noreply.github.com>
2023-01-30 23:02:11 +0530

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -453,8 +453,8 @@ cmd({
             citel.react("⬇️");
             citel.reply("*I Am Downloading Your Song.*");
                 let search = await yts(text);
-            citel.reply("*I Am Uploading Your Song.*");
             citel.react("⬆️");
+            citel.reply("*I Am Uploading Your Song.*");
             await Void.sendMessage(citel.chat, 
     { audio: fs.readFileSync(`./${randomName}`), mimetype: 'audio/mp4' },{quoted: citel})
             } else {
