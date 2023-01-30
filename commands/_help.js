08dc0be561094779f129f5bc648d45c52686e48b
SL Prabath Official <106251140+prabathLK@users.noreply.github.com>
2023-01-30 19:46:08 +0530

    Update _help.js

--- commands/_help.js
+++ commands/_help.js
@@ -13,7 +13,7 @@ const os = require('os')
 const moment = require("moment-timezone")
 const fs = require("fs")
 const Config = require('../config')
-let { fancytext, tlang, typewriter, runtime, formatp, botpic, prefix, sck1 } = require("../lib");
+let { fancytext, tlang, tiny, runtime, formatp, botpic, prefix, sck1 } = require("../lib");
 const long = String.fromCharCode(8206)
 const readmore = long.repeat(4001)
 const Secktor = require('../lib/commands')
@@ -66,7 +66,7 @@ Secktor.cmd({
                 for (const category in cmds) {
                     str += `
 ┃  ╭──────────────❖
-┃  │  ${typewriter(category)} 
+┃  │  ${37(category)} 
 ┃  ╰┬─────────────❖
 ┃  ┌┤\n`
                     for (const plugins of cmds[category]) {
