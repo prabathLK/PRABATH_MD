5652acfe3ea2eb0bbe7375a2b9da2c2f295bdf6a
SL Prabath Official <106251140+prabathLK@users.noreply.github.com>
2023-01-31 12:45:55 +0530

    Update Instagram.js

--- commands/Instagram.js
+++ commands/Instagram.js
@@ -1,4 +1,4 @@
-const { fbdl,cmd } = require('../lib')
+const { Insta,cmd } = require('../lib')
 cmd({
         pattern: "fb",
         react: "ℹ️",
@@ -8,7 +8,7 @@ cmd({
     },
     async(Void, citel,text,{isCreator}) => {
 if(!text) return citel.reply('Need facebook url.')
-let response = await fb(text)
+let response = await Insta(text)
 for (let i=0;i<response.length;i++) {
 await Void.sendFileUrl(citel.chat, response[i], `*Downloaded Media from instagram.*`, citel)
 }
