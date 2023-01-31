330b8ef96158648250f481d78dbf2111c4125e3b
SL Prabath Official <106251140+prabathLK@users.noreply.github.com>
2023-01-31 11:22:41 +0530

    Update Instagram.js

--- commands/Instagram.js
+++ commands/Instagram.js
@@ -12,3 +12,31 @@ for (let i=0;i<response.length;i++) {
 await Void.sendFileUrl(citel.chat, response[i], `*Downloaded Media from instagram.*`, citel)
 }
     });
+Module({
+    pattern: "fb",
+    desc: "Facebook video downloader",
+    category: "downloader",
+    filename: __filename
+}, (async (msg, query) => {
+     var q = !msg.reply_message.message ? query[1] : msg.reply_message.message
+     let _q = !msg.reply_message.message ? query[1] : msg.reply_message.message
+     if (/\bhttps?:\/\/\S+/gi.test(q)) q = q.match(/\bhttps?:\/\/\S+/gi)[0]
+     if (!q) return await msg.sendReply("*Need Facebook link*")
+     if (!_q.includes("!")){
+const buttons = [
+  {buttonId: hnd+'fb '+q+' !hd', buttonText: {displayText: 'HD'}, type: 1},
+  {buttonId: hnd+'fb '+q+' !sd', buttonText: {displayText: 'SD'}, type: 1}
+ ]
+const buttonMessage = {
+    text: "*Select video quality*",
+    footer: '',
+    buttons: buttons,
+    headerType: 1
+}
+ await msg.client.sendMessage(msg.jid, buttonMessage,{quoted:msg.data})
+     }
+    if (_q.includes("!")){
+     var res = await fb(q);
+     return await msg.sendReply({url: res[_q.split("!")[1]]},"video")
+     }
+        }));
