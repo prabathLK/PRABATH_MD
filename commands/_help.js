1d0c8d2b3d3e673d346c213445301f6d1f7f5009
SL Prabath Official <106251140+prabathLK@users.noreply.github.com>
2023-02-01 09:44:09 +0530

    Update _help.js

--- commands/_help.js
+++ commands/_help.js
@@ -70,7 +70,7 @@ Secktor.cmd({
      ╰─────────────────╯
        \n`
                     for (const plugins of cmds[category]) {
-                        str += ` ⦁ ${plugins}\n`
+                        str += `     ⦁ ${plugins}\n`
                     }
                     str += `     `
                 }
