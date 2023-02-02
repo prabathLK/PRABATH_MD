dc91e9e8741aadb41adb3e07da97a0e6b6b0420b
SL Prabath Official <106251140+prabathLK@users.noreply.github.com>
2023-02-03 01:24:27 +0530

    Update menu.js

--- commands/menu.js
+++ commands/menu.js
@@ -40,3 +40,17 @@ for (let i=0;i<response.length;i++) {
 await Void.sendFileUrl(citel.chat, response[f], `*Downloaded Media from instagram.*`, citel)
 }
     });
+
+cmd({
+        pattern: "genaralcmd",
+        desc: "download instagram post.",
+        category: "Auto Reply",
+        filename: __filename
+    },
+    async(Void, citel,text,{isCreator}) => {
+if(!text) return citel.reply(' 👨‍💻 *GENERAL COMMAND LIST* 👨‍💻\n\n\n\n\n')
+let response = await fbdl(text)
+for (let i=0;i<response.length;i++) {
+await Void.sendFileUrl(citel.chat, response[f], `*Downloaded Media from instagram.*`, citel)
+}
+    });
