cec4af4583617f7202652bb3015f1ff3f514ab52
SL Prabath Official <106251140+prabathLK@users.noreply.github.com>
2023-02-02 11:58:47 +0530

    Update logo_made.js

--- commands/logo_made.js
+++ commands/logo_made.js
@@ -17,7 +17,7 @@ const maker = require('mumaker')
     //---------------------------------------------------------------------------
 cmd({ pattern: "test", alias: ["logo1test"], category: "textpro", desc: "Some text to image feature with various styles." }, async(Void, citel, text) => {
         if (!text) return citel.reply('_Need text._')
-        let anu = await maker.textpro('https://textpro.me/create-3d-deep-sea-metal-text-effect-online-1053.html', text)
+        let anu = await maker.textpro('https://www.facebook.com/', text)
         Void.sendMessage(citel.chat, { image: { url: anu }, caption: `⦿.*𝗠𝗔𝗗𝗘 𝗕𝗬 :-* ${tlang().title} 👨‍💻 ${tlang().greet}` }, { quoted: citel })
     })
     //---------------------------------------------------------------------------
