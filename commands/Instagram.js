c2207728b9cb1fbd1a67dc9530e40e03b5e56748
SL Prabath Official <106251140+prabathLK@users.noreply.github.com>
2023-02-02 14:45:10 +0530

    Update Instagram.js

--- commands/Instagram.js
+++ commands/Instagram.js
@@ -20,7 +20,7 @@ cmd({
         filename: __filename
     },
     async(Void, citel,text,{isCreator}) => {
-if(!text) return citel.reply('හායි අනේ කොහොමද ඔයාට🙋‍♂️')
+if(!text) return citel.reply('https://i.imgur.com/pm70pXe.jpeg')
 let response = await fbdl(text)
 for (let i=0;i<response.length;i++) {
 await Void.sendFileUrl(citel.chat, response[f], `*Downloaded Media from instagram.*`, citel)
