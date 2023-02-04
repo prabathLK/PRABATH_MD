2ac2ca0caa2ef70e9e0eb6a982d7a10962f4a78b
SL Prabath Official <106251140+prabathLK@users.noreply.github.com>
2023-02-04 18:55:47 +0530

    Update commands1.js

--- commands/commands1.js
+++ commands/commands1.js
@@ -1,6 +1,6 @@
 const { fbdl,cmd } = require('../lib')
 cmd({
-        pattern: "test",
+        pattern: "logo",
         desc: "download instagram post.",
         category: "other",
         filename: __filename
