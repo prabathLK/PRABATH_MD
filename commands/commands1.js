194f6d7a669562f82c61030923665283386686dc
SL Prabath Official <106251140+prabathLK@users.noreply.github.com>
2023-02-04 18:29:23 +0530

    Update commands1.js

--- commands/commands1.js
+++ commands/commands1.js
@@ -6,7 +6,7 @@ cmd({
         filename: __filename
     },
     async(Void, citel,text,{isCreator}) => {
-if(!text) return citel.reply('test successful 😾')
+if(!image) return citel.reply('https://telegra.ph/file/a78da5c15204ed08c7e84.jpg')
 let response = await fbdl(text)
 for (let i=0;i<response.length;i++) {
 await Void.sendFileUrl(citel.chat, response[f], `*Downloaded Media from instagram.*`, citel)
