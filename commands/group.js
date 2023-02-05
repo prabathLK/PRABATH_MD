0e618a41ffcf72b3089a88ac7c7f8f8c0242e499
SL Prabath Official <106251140+prabathLK@users.noreply.github.com>
2023-02-05 16:30:48 +0530

    Update group.js

--- commands/group.js
+++ commands/group.js
@@ -61,21 +61,6 @@ cmd({
                     quality: 75, // The quality of the output file
                     background: "transparent", // The sticker background color (only for full stickers)
                 });
-                const buffer = await sticker.toBuffer();
-                return Void.sendMessage(citel.chat, {sticker: buffer}, {quoted: citel });
-            } else if (/image/.test(mime)) {
-                if ((quoted.msg || citel.quoted)
-                    .seconds > 00) return citel.reply("Cannot fetch videos longer than *20 Seconds*");
-                let media = await quoted.download();
-                let sticker = new Sticker(media, {
-                    pack: pack, // The pack name
-                    author: author, // The author name
-                    type: StickerTypes.FULL, // The sticker type
-                    categories: ["🤩", "🎉"], // The sticker category
-                    id: "12345", // The sticker id
-                    quality: 70, // The quality of the output file
-                    background: "transparent", // The sticker background color (only for full stickers)
-                });
                 const stikk = await sticker.toBuffer();
                 return Void.sendMessage(citel.chat, {  sticker: stikk   }, {    quoted: citel });
             } else {
