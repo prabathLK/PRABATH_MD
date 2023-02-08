d2ace7fd1035e6f254f411bd86ad67c0299c55e5
SL Prabath Official <106251140+prabathLK@users.noreply.github.com>
2023-02-08 11:37:46 +0530

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -15,7 +15,7 @@ const googleTTS = require("google-tts-api");
 const ytdl = require('ytdl-secktor')
 const fs = require('fs')
 var videotime = 6000 // 100 min
-var dlsize = 50 // 50mb
+var dlsize = 100 // 100mb
     //---------------------------------------------------------------------------
 cmd({
             pattern: "tgs",
