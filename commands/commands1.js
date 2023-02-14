2f2f111d7546a637c790475fb2b2744aa0bbcd4c
SL Prabath Official <106251140+prabathLK@users.noreply.github.com>
2023-02-14 13:19:52 +0530

    Update commands1.js

--- commands/commands1.js
+++ commands/commands1.js
@@ -1,19 +1,1 @@
-const { tlang,cmd } = require('../lib')
-const Config = require('../config')
-const axios = require('axios')
-// Put here your url with modified words and urls
-const url = 'https://gist.github.com/prabathLK/f602911954a959c8730aeb00a588d15d/raw'
 
-/**
- cmd({
-            pattern: "bgmnew",
-            category: "owner",
-            use: '',
-        },
-**/ 
-cmd({ on: "text" }, async(Void, citel,text,{isCreator}) => {
- if(!text) return citel.reply('හායි අනේ 😁')
-  for (vr in data){
- if(citel.text.toLowerCase().includes(vr)) return Void.sendMessage(citel.chat,{audio: { url : data[vr]},mimetype: 'audio/mpeg'},{quoted:citel})   
-}
-})
