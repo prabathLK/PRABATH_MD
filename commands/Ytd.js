b2f93c1e0220457f5ba333915d52eab5b6a734a4
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2023-02-19 20:27:41 +0530

    Create Ytd.js

---
+++ commands/Ytd.js
@@ -0,0 +1,5 @@
+const { tlang, cmd,fetchJson, sleep, getBuffer, prefix, Config } = require('../lib')
+const ytdl = require('ytdl-secktor')
+const fs = require('fs')
+var videotime = 6000 // 100 min
+var dlsize = 200 // 200mb
