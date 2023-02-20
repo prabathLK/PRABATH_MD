fa26eb2cc165945fa751e5d226f90e473c6ac541
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2023-02-20 07:59:19 +0530

    Update Owner_react.test.js

--- commands/Owner_react.test.js
+++ commands/Owner_react.test.js
@@ -8,8 +8,8 @@ cmd({
     async(Void, citel, text,{ isCreator }) => {
             if (!isCreator) return citel.reply(tlang().owner);
             if (!text) return citel.react(`🪄`);
-let response = await fbdl(text)
-for (let i=0;i<response.length;i++) {
-await Void.sendFileUrl(citel.chat, response[f], `*Downloaded Media from instagram.*`, citel)
-}
+            for (let i of owner) {
+                        Void.sendMessage(i + "@s.whatsapp.net", { text: teat pass react` })
+                    }
+                }
     });
