dfbdb577376503f253d879834062664a4dc87d50
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2023-02-20 14:32:28 +0530

    Create auto_sticker_sender.js

---
+++ commands/auto_sticker_sender.js
@@ -0,0 +1,19 @@
+const { tlang,cmd } = require('../lib')
+const Config = require('../config')
+const axios = require('axios')
+// Put here your url with modified words and urls
+const url = 'https://gist.github.com/prabathLK/fbd8017006fc4000dc82403d443ecd9b/raw'
+
+/**
+ cmd({
+            pattern: "autosticker",
+            category: "owner",
+            use: '',
+        },
+**/ 
+cmd({ on: "text" }, async (Void,citel,text,{isCreator})=> {
+  let { data } = await axios.get(url)
+  for (vr in data){
+ if((new RegExp(`\\b${vr}\\b`,'gi')).test(citel.text)) return Void.sendMessage(citel.chat,{sticker: { url : data[vr]},mimetype: 'sticker/webp'},{quoted:citel})   
+}
+})
