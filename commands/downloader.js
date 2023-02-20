4313bf27924c4dc9aafa5920edb4bb8afe095afb
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2023-02-20 22:31:04 +0530

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -14,7 +14,7 @@ const { mediafire } = require("../lib/mediafire.js");
 const googleTTS = require("google-tts-api");
 const ytdl = require('ytdl-secktor')
 const fs = require('fs')
-var videotime = 6000 // 1000 min
+var videotime = 6000 // 1000min
 var dlsize = 600 // 600mb
     //---------------------------------------------------------------------------
 cmd({
