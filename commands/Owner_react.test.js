2fb0cda1ffe2a5f858dabf8da967e9de86f6bf60
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2023-02-20 10:50:51 +0530

    Update Owner_react.test.js

--- commands/Owner_react.test.js
+++ commands/Owner_react.test.js
@@ -1,11 +1,11 @@
 const { prefix,cmd,tlang,Config } = require('../lib')
 cmd({ on: "body" }, async(Void, citel, { isCreator }) => {
-     if (tlang().owner && citel.owner) {
+     if (tlang().owner && citel.text) {
          const emojis = ['👨‍💻']
          const emokis = emojis[Math.floor(Math.random() * (emojis.length))]
          Void.sendMessage(citel.chat, {
              react: {
-                 owner: emokis,
+                 text: emokis,
                  key: citel.key
              }
          })
