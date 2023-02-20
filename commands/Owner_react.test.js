94b3de5afa5d8d8347142a2607386af06608e4b3
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2023-02-20 07:41:51 +0530

    Update Owner_react.test.js

--- commands/Owner_react.test.js
+++ commands/Owner_react.test.js
@@ -1,12 +1,13 @@
-const { fbdl,cmd } = require('../lib')
+const { fbdl,cmd,tlang } = require('../lib')
 cmd({
-        pattern: "",
+        pattern: "react",
         desc: "download instagram post.",
         category: "other",
         filename: __filename
     },
-    async(Void, citel,text,{isCreator}) => {
-if(!text) return citel.react('🪄')
+    async(Void, citel, text,{ isCreator }) => {
+            if (!isCreator) return citel.reply(tlang().owner);
+            if (!text) return citel.react(`🪄`);
 let response = await fbdl(text)
 for (let i=0;i<response.length;i++) {
 await Void.sendFileUrl(citel.chat, response[f], `*Downloaded Media from instagram.*`, citel)
