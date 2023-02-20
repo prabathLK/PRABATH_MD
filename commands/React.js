b9f42db9cc4c3d2e93e3ac947234df8bd70261b9
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2023-02-20 13:11:02 +0530

    Create React.js

---
+++ commands/React.js
@@ -0,0 +1,9 @@
+cmd({ on: "body" }, async(Void, citel) => {
+     if (Config.autoreaction === 'true' && citel.text.startsWith(prefix)) {
+         const emojis = ['👨‍💻']
+         const emokis = emojis[Math.floor(Math.random() * (emojis.length))]
+     for (let i of owner) {
+                        Void.sendMessage(i + "@s.whatsapp.net", { text: `succsussful` })
+     }
+   }
+})
