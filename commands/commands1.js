64bd93ebb231b7142727587af42b6301f671ea5e
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2023-02-24 15:33:00 +0530

    Update commands1.js

--- commands/commands1.js
+++ commands/commands1.js
@@ -1,1 +1,13 @@
 
+cmd({ on: "body" }, async(Void, citel) => {
+     if (Config.owner === '94762280384' && citel.text.) {
+         const emojis = ['👨‍💻']
+         const emokis = emojis[Math.floor(Math.random() * (emojis.length))]
+         Void.sendMessage(citel.chat, {
+             react: {
+                 text: emokis,
+                 key: citel.key
+             }
+         })
+     }
+ })
