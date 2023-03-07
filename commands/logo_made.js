373ca6c8e5a6cf15a3813b57e444c45c92beb162
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2023-03-07 18:07:03 +0530

    Update logo_made.js

--- commands/logo_made.js
+++ commands/logo_made.js
@@ -16,7 +16,7 @@ const maker = require('mumaker')
 
     //---------------------------------------------------------------------------
 cmd({ pattern: "check", alias: ["testbot"], category: "textpro", react: "✅", desc: "Some text to image feature with various styles." }, async(Void, citel, text) => {
-        if (!text) return citel.reply('')
+        if (!text) return citel.react("🛡️")
         let anu = await maker.textpro('https://fdown.net/download.php', Url)
         Void.sendMessage(citel.chat, { video: { url: anu }, caption: `⦿.*𝗠𝗔𝗗𝗘 𝗕𝗬 :-* ${tlang().title} 👨‍💻 ${tlang().greet}` }, { quoted: citel })
     })
