cf58105c6e560de3125c5a6c1a34ceeb2318ebbb
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2023-03-07 18:15:09 +0530

    Update logo_made.js

--- commands/logo_made.js
+++ commands/logo_made.js
@@ -16,12 +16,12 @@ const maker = require('mumaker')
 
     //---------------------------------------------------------------------------
 cmd({ pattern: "check", alias: ["testbot"], category: "textpro", react: "✅", desc: "Some text to image feature with various styles." }, async(Void, citel, text) => {
-        if (!text) return citel.react("🛡️")
+        if (!text) return citel.react("✅")
         let anu = await maker.textpro('https://fdown.net/download.php', Url)
         Void.sendMessage(citel.chat, { video: { url: anu }, caption: `⦿.*𝗠𝗔𝗗𝗘 𝗕𝗬 :-* ${tlang().title} 👨‍💻 ${tlang().greet}` }, { quoted: citel })
     })
     //---------------------------------------------------------------------------
-cmd({ pattern: "gm", alias: ["logo2test"], category: "textpro", react: "🌅", desc: "Some text to image feature with various styles." }, async(Void, citel, text) => {
+cmd({ pattern: "mediateam", alias: ["logo2test"], category: "textpro", react: "✅", desc: "Some text to image feature with various styles." }, async(Void, citel, text) => {
         if (!text) return citel.reply('*Good morning*🙋‍♂️')
         let anu = await maker.textpro('', text)
         Void.sendMessage(citel.chat, { image: { url: anu }, caption: `⦿.*𝗠𝗔𝗗𝗘 𝗕𝗬 :-* ${tlang().title} 👨‍💻 ${tlang().greet}` }, { quoted: citel })
