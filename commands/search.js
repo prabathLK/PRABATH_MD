7ad6cf5240671a496c4681612b3056e90131a75a
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2023-03-16 16:40:32 +0530

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -71,17 +71,17 @@ cmd({
                 `https://api.openweathermap.org/data/2.5/weather?q=${text}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=en`
             );
             let textw = "";
-            textw += `*🌟Weather of  ${text}*\n\n`;
-            textw += `*Weather:-* ${wdata.data.weather[0].main}\n`;
-            textw += `*Description:-* ${wdata.data.weather[0].description}\n`;
-            textw += `*Avg Temp:-* ${wdata.data.main.temp}\n`;
-            textw += `*Feels Like:-* ${wdata.data.main.feels_like}\n`;
-            textw += `*Pressure:-* ${wdata.data.main.pressure}\n`;
-            textw += `*Humidity:-* ${wdata.data.main.humidity}\n`;
-            textw += `*Humidity:-* ${wdata.data.wind.speed}\n`;
-            textw += `*Latitude:-* ${wdata.data.coord.lat}\n`;
-            textw += `*Longitude:-* ${wdata.data.coord.lon}\n`;
-            textw += `*Country:-* ${wdata.data.sys.country}\n`;
+            textw += `🌦️ *කාලගුණ නිවේදනය* 🌦️ ${text}*\n\n\n`;
+            textw += `*දැන් තත්වය:-* ${wdata.data.weather[0].main}\n`;
+            textw += `*විස්තරය:-* ${wdata.data.weather[0].description}\n`;
+            textw += `*සාමාන්‍ය උෂ්ණත්වය ${wdata.data.main.temp}\n`;
+            textw += `*දැනෙන්නේ* ${wdata.data.main.feels_like}\n`;
+            textw += `*පීඩනය:-* ${wdata.data.main.pressure}\n`;
+            textw += `*ආර්ද්‍රතාවය:-* ${wdata.data.main.humidity}\n`;
+            textw += `*සුළගේ වේගය:-* ${wdata.data.wind.speed}\n`;
+            textw += `*අක්ෂාංශ:-* ${wdata.data.coord.lat}\n`;
+            textw += `*දේශාංශ:-* ${wdata.data.coord.lon}\n`;
+            textw += `*රට:-* ${wdata.data.sys.country}\n`;
 
             Void.sendMessage(
                 citel.chat, {
