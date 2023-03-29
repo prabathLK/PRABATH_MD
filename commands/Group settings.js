bae407295875837a7bb5acdee9786d96bb6c0880
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2023-03-29 21:03:39 +0530

    Update Group settings.js

--- commands/Group settings.js
+++ commands/Group settings.js
@@ -4,159 +4,66 @@ const { cmd,tlang,prefix,runtime,formatp} = require('../lib/');
 
 cmd({
 
-            pattern: "groupsettings",
+            pattern: "gs",
 
-            alias: ["gs"],
+            desc: "(menu cmdlist).",
 
-            desc: "some group settings change.",
+            category: "downloader",
 
-            category: "extra",
+            react: "⚙️",
 
-            filename: __filename
+            filename: __filename,
+
+            use: '<faded-Alan walker.>',
 
         },
 
         async(Void, citel, text) => {
 
-           let list = [{
+            let buttonMessage = {
 
-title: 'MUTE GROUP 🔇',
+                image: {
 
-rowId: `${prefix}group close`,
+                    url: await botpic(),
 
-description: ' '
+                },
 
-},
+                caption: `
 
-{
-
-title: 'UNMUTE GROUP 🔊',
-
-rowId: `${prefix}group open`,
-
-description: ' '
-
-},
-
-{
-
-title: 'ACTIVE ANTILINK ✅',
-
-rowId: `${prefix}act antilink`,
-
-description: ' '
-
-},
-
-{
-
-title: 'DEACTIVE ANTILINK ❌',
-
-rowId: `${prefix}deact antilink`,
-
-description: ' '
-
-},
-
-{
-
-title: 'ACTIVE BOT THIS GROUP ✅',
-
-rowId: `${prefix}bot on`,
-
-description: ''
-
-},
-
-{
-
-title: 'DEACTIVE BOT THIS GROUP ❌',
-
-rowId: `${prefix}bot off`,
-
-description: ''
-
-},
-
-{
-
-title: 'ACTIVE ADD MESSAGE AND LEFT MESSAGE ✅',
-
-rowId: `${prefix}act events`,
-
-description: ''
-
-},
-
-{
-
-title: 'DEACTIVE ADD MESSAGE AND LEFT MESSAGE ❌',
-
-rowId: `${prefix}deact events`,
-
-description: ''
-
-}
-
-            ]
-
-            ted = `┏━━━━━━━━━━━━━━━━━━━━━━━━━
-┃   *GROUP SETTINGS CHANGE*
+┏━━━━━━━━━━━━━━━━━━━━━━━━━
+┃  *GROUP SETTINGS CHANGE*
 ┗━━━━━━━━━━━━━━━━━━━━━━━━━
 
 
-*THINGS THAT CAN BE CHANGED FROM THIS,*
-
-● MUTE GROUP
-● UNMUTE GROUP
-
-● ACTIVE ANTILINK
-● DEACTIVE ANTILINK
-
-● ACTIVE BOT THIS GROUP
-● DEACTIVE BOT THIS GROUP
-
-● ACTIVE ADD MESSAGE AND LEFT MESSAGE
-● DEACTIVE ADD MESSAGE AND LEFT MESSAGE 
+🔇 *MUTE GROUP -* .group close
+🔊 *UNMUTE GROUP -* .group open
 
 
-⦿. *REQUEST BY:* ${citel.pushName}
+✅ *ACTIVE ANTILINK -* .act antilink
+❌ *DEACTIVE ANTILINK -* deact antilink
 
-`
 
-            const sections = [
+✅ *ACTIVE BOT THIS GROUP -* .bot on
+❌ *DEACTIVE BOT THIS GROUP -* .bot off
 
-                {
 
-                    title: "Change settings",
+✅ *ACTIVE ADD AND LEFT MSJ -* .act events
+❌ *DEACTIVE ADD AND LEFT MSJ -* .deact events
 
-                    rows: list
-
-                }
-
-            ]
-
-            const listMessage = {
-
-                text: ted,
+`,
 
                 footer: tlang().footer,
 
-                title: ``,
 
-                buttonText: "Change Settings ",
+                headerType: 4,
 
-                mentions: await Void.parseMention(ted),
+            };
 
-                sections
+            return Void.sendMessage(citel.chat, buttonMessage, {
 
-            }
+                quoted: citel,
 
-            return Void.sendMessage(citel.chat, listMessage, {
-
-                quoted: citel
-
-            })
+            });
 
         }
 
