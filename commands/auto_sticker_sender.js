544d61408a673fe44cd67ef66f41fe2982eaf030
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2023-04-01 12:16:52 +0530

    Update auto_sticker_sender.js

--- commands/auto_sticker_sender.js
+++ commands/auto_sticker_sender.js
@@ -1,4 +1,4 @@
-const { tlang,cmd,prefix } = require('../lib')
+const { tlang,cmd } = require('../lib')
 const { Sticker, createSticker, StickerTypes } = require("wa-sticker-formatter");
 const Config = require('../config')
 const axios = require('axios')
@@ -13,7 +13,7 @@ const url = 'https://gist.github.com/prabathLK/fbd8017006fc4000dc82403d443ecd9b/
         },
 **/ 
 cmd({ on: "body" }, async (Void,citel,text)=> {
- if (Config.autosticker === 'false' && citel.text);
+ if (Config.autosticker === 'false' && citel.text.startsWith(prefix));
   let { data } = await axios.get(url)
   for (vr in data){
  if((new RegExp(`\\b${vr}\\b`,'gi')).test(citel.text)) await Void.sendMessage(citel.chat,{sticker: { url : data[vr]},package: 'made by prabath'},{ quoted: citel })   
