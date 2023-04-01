b60346dc1f2096d5ab298df877b204e14a5ee16a
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2023-04-01 11:13:58 +0530

    Update Auto_Voice.js

--- commands/Auto_Voice.js
+++ commands/Auto_Voice.js
@@ -24,7 +24,7 @@ const url = 'https://gist.github.com/prabathLK/f602911954a959c8730aeb00a588d15d/
 
 cmd({ on: "text" }, async (Void,citel,text,{isCreator})=> {
 
-if (Config.autoreaction === 'true');
+if (Config.autovoice === 'true');
 
   let { data } = await axios.get(url)
 
