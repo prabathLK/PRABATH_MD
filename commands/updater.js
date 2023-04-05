ef674b680ee1bb209901ed0bb39dd6b7d8bb2b47
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2023-04-05 22:46:02 +0530

    Create updater.js

---
+++ commands/updater.js
@@ -0,0 +1,53 @@
+const DB = require('../lib/scraper')
+
+const { execSync } = require('child_process')
+
+const { tlang, Config, prefix,cmd } = require('../lib')
+
+    //---------------------------------------------------------------------------
+
+cmd({
+
+            pattern: "update",
+
+            desc: "Shows repo\'s refreshed commits.",
+
+            category: "misc",
+
+            filename: __filename
+
+        },
+
+        async(Void, citel, text,{ isCreator }) => {
+
+            if (!isCreator) return citel.reply('*OWNER CMD* ⛔')
+
+            let commits = await DB.syncgit()
+
+            if (commits.total === 0) {
+
+                citel.reply(`*Hey ${citel.pushName}. You have latest version installed.* ✅`)
+
+            } else {
+
+                let update = await DB.sync()
+
+                  let buttonMessaged = {
+
+                    text: update,
+
+                    footer: 'UPDATER',
+
+                    headerType: 4
+
+                };
+
+                return await Void.sendMessage(citel.chat, buttonMessaged);
+
+            }
+
+        }
+
+    )
+
+  
