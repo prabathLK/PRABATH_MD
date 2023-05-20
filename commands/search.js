653c429576943ff9e66523a3c89b0d1f1f829f97
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2023-05-20 09:36:34 +0530

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -155,41 +155,6 @@ cmd({
     )
     //---------------------------------------------------------------------------
 cmd({
-            pattern: "img",
-            alias: ["image"],
-            category: "search",
-            desc: "Searches Image on Google",
-            use: '<text>',
-            filename: __filename,
-        },
-        async(Void, citel, text) => {
-            if (!text) return citel.reply("Provide me a query!")
-            if (!text) return reply("Hey bie please tell me for which pic you're looking");
-            let name1 = text.split("|")[0]
-            let name2 = text.split("|")[5] || `5`
-            citel.reply(`*Sending ${name2} image(s) of ${name1} in chat*`)
-            let nn = name2
-            for (let i = 0; i < nn; i++) {
-
-                gis(name1, async(error, result) => {
-                    n = result;
-                    images = n[Math.floor(Math.random() * n.length)].url;
-                    let buttonMessage = {
-                        image: {
-                            url: images,
-                        },
-                        caption: tlang().footer,
-                        headerType: 4,
-                    };
-                    Void.sendMessage(citel.chat, buttonMessage, {
-                        quoted: citel,
-                    });
-                })
-            }
-        }
-    )
-    //---------------------------------------------------------------------------
-cmd({
             pattern: "couplepp",
             category: "search",
             desc: "Sends two couples pics.",
