64d543be2fd7cb4987bc2b5a5f49287ad592f806
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2023-06-11 18:17:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -92,7 +92,7 @@ let file = require.resolve(__filename)
 
 fs.watchFile(file, () => {
 
-	fs.unwatchFile(file)	console.log(`Update'${__filename}'`)
+	fs.unwatchFile(file)	console.log(`Update ${__filename}`)
 
     delete require.cache[file]
 
